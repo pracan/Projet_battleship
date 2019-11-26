52c8021018091dea6caabc2d399a1af825600bcc
pracan <rajamaxime@eisti.eu>
2019-11-26 08:54:03 +0000

    Version 1

--- main.c
+++ main.c
@@ -10,8 +10,9 @@
 
 void creerGrille (int L, int H, int **grille) {
 	int o;
-	
-	for (int i=0; i<=L-1;i++){
+  int a;
+
+	for (int i=1; i<=L-1;i++){
 		for (int j=0;j<=H-1;j++) {
 			grille[i][j]=0;
 		}
@@ -21,7 +22,12 @@ void creerGrille (int L, int H, int **grille) {
 
 void afficherGrille(int L, int H, int **grille){
 	
-	for (int i=0; i<=L-1;i++) {
+  for (int i=0; i<L;i++) {
+    printf("%d ",i+1);
+  }
+  printf("\n");
+
+	for (int i=1; i<=L-1;i++) {
 		for (int j=0; j<=H-1;j++){
 			if (grille[i][j]==0) {
 				printf(". ");
@@ -32,8 +38,6 @@ void afficherGrille(int L, int H, int **grille){
 		}
 	printf("\n");
 	}
-
-  printf(grille[1[0]);
 }
 
 int main(int argc, char **argv) {
@@ -44,7 +48,7 @@ int main(int argc, char **argv) {
 	scanf("%d",&L);
 	printf("Hauteur ?\n");
 	scanf("%d",&H);
-		
+
 	grille = malloc(L*sizeof(int*));
 	for (int i=0;i<=H;i++){
 		grille[i]=malloc(H*sizeof(int));
